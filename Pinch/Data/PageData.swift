e9724020413584c2e7a4869ebe0b67be744b4851
PavanSisode <pavansisode@gmail.com>
2022-05-25 17:25:35 +0530

    initial commit

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,12 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Pavan Shisode on 27/04/22.
+//
+
+import Foundation
+let pagesData: [Page] = [
+Page(id: 1, imageName: "magazine-front-cover"),
+Page(id: 2, imageName: "magazine-back-cover"),
+]
